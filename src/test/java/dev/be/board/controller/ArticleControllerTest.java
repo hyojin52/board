84395bd02133ecee5ffbd25ac2cfe8908c37c3b4
hyojin <dongurami.kim@gmail.com>
2024-07-05 16:23:35 +0900

    #14 - 게시판 컨트롤러 구현
    게시판 페이지 기본 정렬 속성 부여: 10개 1페이지, 시간 내림차순

--- src/test/java/dev/be/board/controller/ArticleControllerTest.java
+++ src/test/java/dev/be/board/controller/ArticleControllerTest.java
@@ -1,21 +1,40 @@
 package dev.be.board.controller;
 
+import dev.be.board.config.SecurityConfig;
+import dev.be.board.dto.ArticleWithCommentsDto;
+import dev.be.board.dto.UserAccountDto;
+import dev.be.board.service.ArticleService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
-@WebMvcTest
+@WebMvcTest(ArticleController.class)
+@Import(SecurityConfig.class)
 class ArticleControllerTest {
   
   private final MockMvc mockMvc;
+  
+  @MockBean
+  private ArticleService articleService;
   
   ArticleControllerTest(
           @Autowired MockMvc mockMvc
@@ -27,6 +46,7 @@ class ArticleControllerTest {
   @Test
   public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView () throws Exception {
     // Given
+    given(articleService.searchArticles(eq(null), eq(null), any(Pageable.class))).willReturn(Page.empty());
     
     // When & Then
     mockMvc.perform(get("/articles"))
@@ -34,12 +54,16 @@ class ArticleControllerTest {
             .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
             .andExpect(view().name("articles/index"))
             .andExpect(model().attributeExists("articles"));
+    
+    then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
   }
   
   @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
   @Test
   public void givenNothing_whenRequestingArticleView_thenReturnsArticleView () throws Exception {
     // Given
+    Long articleId = 1L;
+    given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
     
     // When & Then
     mockMvc.perform(get("/articles/1"))
@@ -47,6 +71,7 @@ class ArticleControllerTest {
             .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
             .andExpect(view().name("articles/detail"))
             .andExpect(model().attributeExists("article"));
+    then(articleService).should().getArticle(articleId);
   }
   
   @Disabled("개발중")
@@ -71,5 +96,35 @@ class ArticleControllerTest {
     mockMvc.perform(get("/articles/search-hashtag"))
             .andExpect(status().isOk())
             .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+  }
+  
+  private ArticleWithCommentsDto createArticleWithCommentsDto() {
+    return ArticleWithCommentsDto.of(
+            1L,
+            createUserAccountDto(),
+            Set.of(),
+            "title",
+            "content",
+            "#jave",
+            LocalDateTime.now(),
+            "uno",
+            LocalDateTime.now(),
+            "uno"
+    );
+  }
+  
+  private UserAccountDto createUserAccountDto() {
+    return UserAccountDto.of(
+            1L,
+            "uno",
+            "pw",
+            "uno@email.com",
+            "Uno",
+            "memo",
+            LocalDateTime.now(),
+            "uno",
+            LocalDateTime.now(),
+            "uno"
+    );
   }
 }
